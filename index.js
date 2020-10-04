89f4e76e14349f4a64af2f9ea748b708666cece8
Jessica Salbert <jessicarsalbert@gmail.com>
2020-10-04 19:45:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solutions in this file
+<<<<<<< HEAD
 // Code your solutions in this file
 function writeCards(array, event)  {
     let gifts = [];
@@ -14,3 +15,19 @@ function writeCards(array, event)  {
         number --
       }
     }
+=======
+function writeCards(array, event)  {
+  let gifts = [];
+  for (let i = 0; i < array.length; i++) {
+    gifts.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return gifts
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number)
+      number --
+    }
+  }
+>>>>>>> 7f9f7ac193a6470e2d8e5ddaac2f812d3bfa8ecc
